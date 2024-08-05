a95703d66ec4b0338d3d878f3ef094153b4ef389
hyjwpk <74606714+hyjwpk@users.noreply.github.com>
2024-08-05 23:02:53 +0800

    图片并行推理
    - 并行推理 1w 张图片，将图片的维度加到 block 上
    - 将读取的图片保存为 float*，以便快速 Memcpy
    - 修改 Linear 的 thread 分配，将输出的维度从 block 改到 thread 上

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ GPU   	NVIDIA GeForce RTX 2060
 | torch cpu baseline | 0.3000s |
 | torch gpu baseline | 0.0150s |
 |   cuda baseline    | 6.3073s |
+|    图片并行推理    | 0.0527s |
 
 ## prof 结果
 
@@ -43,6 +44,30 @@ Profiling result:
                     0.00%     200ns         1     200ns     200ns     200ns  cuDeviceGetUuid
 ```
 
+### 图片并行推理
 
+```bash
+Profiling application: ./infer
+Profiling result:
+            Type  Time(%)      Time     Calls       Avg       Min       Max  Name
+ GPU activities:   46.80%  24.829ms         3  8.2763ms  278.56us  20.942ms  Linear_ReLu(float*, float*, float*, float*, int, int, bool)
+                   35.33%  18.744ms         2  9.3718ms  8.6073ms  10.136ms  Conv_ReLu(float*, float*, float*, float*, int, int, int, int, int)
+                   15.58%  8.2652ms        11  751.38us  1.3120us  8.2264ms  [CUDA memcpy HtoD]
+                    2.19%  1.1606ms         2  580.31us  456.32us  704.29us  MaxPool(float*, float*, int, int, int)
+                    0.11%  60.673us         1  60.673us  60.673us  60.673us  [CUDA memcpy DtoH]
+      API calls:   94.96%  1.41670s        18  78.706ms  3.3000us  1.40981s  cudaMalloc
+                    3.09%  46.046ms         7  6.5780ms  432.90us  21.101ms  cudaDeviceSynchronize
+                    0.99%  14.792ms        12  1.2327ms  62.100us  8.0592ms  cudaMemcpy
+                    0.66%  9.9202ms        18  551.12us  3.3000us  3.7833ms  cudaFree
+                    0.29%  4.2612ms         1  4.2612ms  4.2612ms  4.2612ms  cuDeviceGetPCIBusId
+                    0.01%  166.60us         7  23.800us  8.7000us  43.600us  cudaLaunchKernel
+                    0.00%  24.900us       101     246ns     100ns  1.5000us  cuDeviceGetAttribute
+                    0.00%  4.2000us         7     600ns     200ns  1.1000us  cudaGetLastError
+                    0.00%  2.1000us         3     700ns     200ns  1.1000us  cuDeviceGetCount
+                    0.00%  1.6000us         2     800ns     200ns  1.4000us  cuDeviceGet
+                    0.00%  1.4000us         1  1.4000us  1.4000us  1.4000us  cuDeviceGetName
+                    0.00%     700ns         1     700ns     700ns     700ns  cuDeviceTotalMem
+                    0.00%     600ns         1     600ns     600ns     600ns  cuModuleGetLoadingMode
+                    0.00%     300ns         1     300ns     300ns     300ns  cuDeviceGetUuid
+```
 
-​      
--- infer.cu
+++ infer.cu
@@ -19,7 +19,7 @@
 
 #define OFFSET(row, col, ld) ((row) * (ld) + (col))
 
-std::vector<std::vector<float>> read_mnist_images(const std::string &path) {
+float *read_mnist_images(const std::string &path) {
     std::ifstream file(path, std::ios::binary);
     if (!file) {
         std::cout << "Cannot open file!" << std::endl;
@@ -43,13 +43,13 @@ std::vector<std::vector<float>> read_mnist_images(const std::string &path) {
                ((num_cols & 0x0000ff00) << 8) | ((num_cols & 0x000000ff) << 24);
 
     int image_size = num_rows * num_cols;
-    std::vector<std::vector<float>> images(num_images, std::vector<float>(image_size));
+    float *images = (float *)malloc(num_images * image_size * sizeof(float));
 
     for (int i = 0; i < num_images; ++i) {
         for (int j = 0; j < image_size; ++j) {
             unsigned char pixel = 0;
             file.read((char *)&pixel, sizeof(pixel));
-            images[i][j] = static_cast<float>(pixel) / 255.0f;
+            images[OFFSET(i, j, image_size)] = static_cast<float>(pixel) / 255.0f;
         }
     }
 
@@ -93,41 +93,41 @@ std::vector<float> read_param(const std::string &path) {
     return params;
 }
 
-__global__ void Conv_ReLu(float *input_image, float *output_image, float *kernel_weights, float *kernel_bias, int input_height, int kernel_height, int output_height, int input_channel) {
+__global__ void Conv_ReLu(float *input_image, float *output_image, float *kernel_weights, float *kernel_bias, int input_height, int kernel_height, int output_height, int input_channel, int output_channel) {
     float value = 0;
     for (int z = 0; z < input_channel; z++) {
         for (int i = 0; i < kernel_height; i++) {
             for (int j = 0; j < kernel_height; j++) {
-                value += input_image[z * input_height * input_height + OFFSET(threadIdx.y + i, threadIdx.x + j, input_height)] * kernel_weights[blockIdx.x * input_channel * kernel_height * kernel_height + z * kernel_height * kernel_height + OFFSET(i, j, kernel_height)];
+                value += input_image[blockIdx.y * input_channel * input_height * input_height + z * input_height * input_height + OFFSET(threadIdx.y + i, threadIdx.x + j, input_height)] * kernel_weights[blockIdx.x * input_channel * kernel_height * kernel_height + z * kernel_height * kernel_height + OFFSET(i, j, kernel_height)];
             }
         }
     }
     value += kernel_bias[blockIdx.x];
     value = value > 0 ? value : 0;
-    output_image[blockIdx.x * output_height * output_height + OFFSET(threadIdx.y, threadIdx.x, output_height)] = value;
+    output_image[blockIdx.y * output_channel * output_height * output_height + blockIdx.x * output_height * output_height + OFFSET(threadIdx.y, threadIdx.x, output_height)] = value;
 }
 
-__global__ void MaxPool(float *input_image, float *output_image, int input_height, int output_height) {
+__global__ void MaxPool(float *input_image, float *output_image, int input_height, int output_height, int channel) {
     float value = 0;
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
-            int input_image_index = blockIdx.x * input_height * input_height + OFFSET(threadIdx.y * 2 + i, threadIdx.x * 2 + j, input_height);
+            int input_image_index = blockIdx.y * channel * input_height * input_height + blockIdx.x * input_height * input_height + OFFSET(threadIdx.y * 2 + i, threadIdx.x * 2 + j, input_height);
             if (input_image[input_image_index] > value) {
                 value = input_image[input_image_index];
             }
         }
     }
-    output_image[blockIdx.x * output_height * output_height + threadIdx.y * output_height + threadIdx.x] = value;
+    output_image[blockIdx.y * channel * output_height * output_height + blockIdx.x * output_height * output_height + threadIdx.y * output_height + threadIdx.x] = value;
 }
 
-__global__ void Linear_ReLu(float *input_image, float *output_image, float *kernel_weights, float *kernel_bias, int input_height, bool ReLu) {
+__global__ void Linear_ReLu(float *input_image, float *output_image, float *kernel_weights, float *kernel_bias, int input_height, int output_height, bool ReLu) {
     float value = 0;
     for (int i = 0; i < input_height; i++) {
-        value += input_image[i] * kernel_weights[OFFSET(blockIdx.x, i, input_height)];
+        value += input_image[blockIdx.x * input_height + i] * kernel_weights[OFFSET(threadIdx.x, i, input_height)];
     }
-    value += kernel_bias[blockIdx.x];
+    value += kernel_bias[threadIdx.x];
     value = ((!ReLu) || (value > 0)) ? value : 0;
-    output_image[blockIdx.x] = value;
+    output_image[blockIdx.x * output_height + threadIdx.x] = value;
 }
 
 int main(int argc, char *argv[]) {
@@ -146,6 +146,7 @@ int main(int argc, char *argv[]) {
     auto Linear3_bias = read_param("./weight/fc3.bias.txt");
 
     // Conv1 [1, 28, 28] -> [6, 24, 24]
+    const int batchsize = 10000;
     const int Conv1_input_height = 28;
     const int Conv1_kernel_height = 5;
     const int Conv1_output_height = Conv1_input_height - Conv1_kernel_height + 1;
@@ -155,13 +156,13 @@ int main(int argc, char *argv[]) {
     float *device_Conv1_output_image;
     float *device_Conv1_kernel_weight;
     float *device_Conv1_kernel_bias;
-    CHECK(cudaMalloc((void **)&device_input_image, Conv1_input_channel * Conv1_input_height * Conv1_input_height * sizeof(float)));
-    CHECK(cudaMalloc((void **)&device_Conv1_output_image, Conv1_output_channel * Conv1_output_height * Conv1_output_height * sizeof(float)));
+    CHECK(cudaMalloc((void **)&device_input_image, batchsize * Conv1_input_channel * Conv1_input_height * Conv1_input_height * sizeof(float)));
+    CHECK(cudaMalloc((void **)&device_Conv1_output_image, batchsize * Conv1_output_channel * Conv1_output_height * Conv1_output_height * sizeof(float)));
     CHECK(cudaMalloc((void **)&device_Conv1_kernel_weight, Conv1_output_channel * Conv1_input_channel * Conv1_kernel_height * Conv1_kernel_height * sizeof(float)));
     CHECK(cudaMalloc((void **)&device_Conv1_kernel_bias, Conv1_output_channel * sizeof(float)));
     CHECK(cudaMemcpy(device_Conv1_kernel_weight, &Conv1_weight[0], Conv1_output_channel * Conv1_input_channel * Conv1_kernel_height * Conv1_kernel_height * sizeof(float), cudaMemcpyHostToDevice));
     CHECK(cudaMemcpy(device_Conv1_kernel_bias, &Conv1_bias[0], Conv1_output_channel * sizeof(float), cudaMemcpyHostToDevice));
-    dim3 Conv1_blocksperGrid(Conv1_output_channel);
+    dim3 Conv1_blocksperGrid(Conv1_output_channel, batchsize);
     dim3 Conv1_threadsperBlock(Conv1_output_height, Conv1_output_height);
 
     // MaxPool1 [6, 24, 24] -> [6, 12, 12]
@@ -169,8 +170,8 @@ int main(int argc, char *argv[]) {
     const int MaxPool1_output_height = MaxPool1_input_height / 2;
     const int MaxPool1_channel = Conv1_output_channel;
     float *device_MaxPool1_output_image;
-    CHECK(cudaMalloc((void **)&device_MaxPool1_output_image, MaxPool1_channel * MaxPool1_output_height * MaxPool1_output_height * sizeof(float)));
-    dim3 MaxPool1_blocksperGrid(MaxPool1_channel);
+    CHECK(cudaMalloc((void **)&device_MaxPool1_output_image, batchsize * MaxPool1_channel * MaxPool1_output_height * MaxPool1_output_height * sizeof(float)));
+    dim3 MaxPool1_blocksperGrid(MaxPool1_channel, batchsize);
     dim3 MaxPool1_threadsperBlock(MaxPool1_output_height, MaxPool1_output_height);
 
     // Conv2 [6, 12, 12] -> [16, 8, 8]
@@ -182,12 +183,12 @@ int main(int argc, char *argv[]) {
     float *device_Conv2_output_image;
     float *device_Conv2_kernel_weight;
     float *device_Conv2_kernel_bias;
-    CHECK(cudaMalloc((void **)&device_Conv2_output_image, Conv2_output_channel * Conv2_output_height * Conv2_output_height * sizeof(float)));
+    CHECK(cudaMalloc((void **)&device_Conv2_output_image, batchsize * Conv2_output_channel * Conv2_output_height * Conv2_output_height * sizeof(float)));
     CHECK(cudaMalloc((void **)&device_Conv2_kernel_weight, Conv2_output_channel * Conv2_input_channel * Conv2_kernel_height * Conv2_kernel_height * sizeof(float)));
     CHECK(cudaMalloc((void **)&device_Conv2_kernel_bias, Conv2_output_channel * sizeof(float)));
     CHECK(cudaMemcpy(device_Conv2_kernel_weight, &Conv2_weight[0], Conv2_output_channel * Conv2_input_channel * Conv2_kernel_height * Conv2_kernel_height * sizeof(float), cudaMemcpyHostToDevice));
     CHECK(cudaMemcpy(device_Conv2_kernel_bias, &Conv2_bias[0], Conv2_output_channel * sizeof(float), cudaMemcpyHostToDevice));
-    dim3 Conv2_blocksperGrid(Conv2_output_channel);
+    dim3 Conv2_blocksperGrid(Conv2_output_channel, batchsize);
     dim3 Conv2_threadsperBlock(Conv2_output_height, Conv2_output_height);
 
     // MaxPool2 [16, 8, 8] -> [16, 4, 4]
@@ -195,8 +196,8 @@ int main(int argc, char *argv[]) {
     const int MaxPool2_output_height = MaxPool2_input_height / 2;
     const int MaxPool2_channel = Conv2_output_channel;
     float *device_MaxPool2_output_image;
-    CHECK(cudaMalloc((void **)&device_MaxPool2_output_image, MaxPool2_channel * MaxPool2_output_height * MaxPool2_output_height * sizeof(float)));
-    dim3 MaxPool2_blocksperGrid(MaxPool2_channel);
+    CHECK(cudaMalloc((void **)&device_MaxPool2_output_image, batchsize * MaxPool2_channel * MaxPool2_output_height * MaxPool2_output_height * sizeof(float)));
+    dim3 MaxPool2_blocksperGrid(MaxPool2_channel, batchsize);
     dim3 MaxPool2_threadsperBlock(MaxPool2_output_height, MaxPool2_output_height);
 
     // Linear1 [256] -> [120]
@@ -205,13 +206,13 @@ int main(int argc, char *argv[]) {
     float *device_Linear1_output_image;
     float *device_Linear1_kernel_weight;
     float *device_Linear1_kernel_bias;
-    CHECK(cudaMalloc((void **)&device_Linear1_output_image, Linear1_output_height * sizeof(float)));
+    CHECK(cudaMalloc((void **)&device_Linear1_output_image, batchsize * Linear1_output_height * sizeof(float)));
     CHECK(cudaMalloc((void **)&device_Linear1_kernel_weight, Linear1_output_height * Linear1_input_height * sizeof(float)));
     CHECK(cudaMalloc((void **)&device_Linear1_kernel_bias, Linear1_output_height * sizeof(float)));
     CHECK(cudaMemcpy(device_Linear1_kernel_weight, &Linear1_weight[0], Linear1_output_height * Linear1_input_height * sizeof(float), cudaMemcpyHostToDevice));
     CHECK(cudaMemcpy(device_Linear1_kernel_bias, &Linear1_bias[0], Linear1_output_height * sizeof(float), cudaMemcpyHostToDevice));
-    dim3 Linear1_blocksperGrid(Linear1_output_height);
-    dim3 Linear1_threadsperBlock(1);
+    dim3 Linear1_blocksperGrid(batchsize);
+    dim3 Linear1_threadsperBlock(Linear1_output_height);
 
     // Linear2 [120] -> [84]
     const int Linear2_input_height = Linear1_output_height;
@@ -219,13 +220,13 @@ int main(int argc, char *argv[]) {
     float *device_Linear2_output_image;
     float *device_Linear2_kernel_weight;
     float *device_Linear2_kernel_bias;
-    CHECK(cudaMalloc((void **)&device_Linear2_output_image, Linear2_output_height * sizeof(float)));
+    CHECK(cudaMalloc((void **)&device_Linear2_output_image, batchsize * Linear2_output_height * sizeof(float)));
     CHECK(cudaMalloc((void **)&device_Linear2_kernel_weight, Linear2_output_height * Linear2_input_height * sizeof(float)));
     CHECK(cudaMalloc((void **)&device_Linear2_kernel_bias, Linear2_output_height * sizeof(float)));
     CHECK(cudaMemcpy(device_Linear2_kernel_weight, &Linear2_weight[0], Linear2_output_height * Linear2_input_height * sizeof(float), cudaMemcpyHostToDevice));
     CHECK(cudaMemcpy(device_Linear2_kernel_bias, &Linear2_bias[0], Linear2_output_height * sizeof(float), cudaMemcpyHostToDevice));
-    dim3 Linear2_blocksperGrid(Linear2_output_height);
-    dim3 Linear2_threadsperBlock(1);
+    dim3 Linear2_blocksperGrid(batchsize);
+    dim3 Linear2_threadsperBlock(Linear2_output_height);
 
     // Linear3 [84] -> [10]
     const int Linear3_input_height = Linear2_output_height;
@@ -234,53 +235,56 @@ int main(int argc, char *argv[]) {
     float *device_Linear3_kernel_weight;
     float *device_Linear3_kernel_bias;
     float *host_Linear3_output_image;
-    CHECK(cudaMalloc((void **)&device_Linear3_output_image, Linear3_output_height * sizeof(float)));
+    CHECK(cudaMalloc((void **)&device_Linear3_output_image, batchsize * Linear3_output_height * sizeof(float)));
     CHECK(cudaMalloc((void **)&device_Linear3_kernel_weight, Linear3_output_height * Linear3_input_height * sizeof(float)));
     CHECK(cudaMalloc((void **)&device_Linear3_kernel_bias, Linear3_output_height * sizeof(float)));
     CHECK(cudaMemcpy(device_Linear3_kernel_weight, &Linear3_weight[0], Linear3_output_height * Linear3_input_height * sizeof(float), cudaMemcpyHostToDevice));
     CHECK(cudaMemcpy(device_Linear3_kernel_bias, &Linear3_bias[0], Linear3_output_height * sizeof(float), cudaMemcpyHostToDevice));
-    host_Linear3_output_image = (float *)malloc(Linear3_output_height * sizeof(float));
-    dim3 Linear3_blocksperGrid(Linear3_output_height);
-    dim3 Linear3_threadsperBlock(1);
+    host_Linear3_output_image = (float *)malloc(batchsize * Linear3_output_height * sizeof(float));
+    dim3 Linear3_blocksperGrid(batchsize);
+    dim3 Linear3_threadsperBlock(Linear3_output_height);
 
     int correct_nums = 0;
     auto start = std::chrono::high_resolution_clock::now();
 
-    for (int t = 0; t < images.size(); t++) {
-        CHECK(cudaMemcpy(device_input_image, &images[t][0], Conv1_input_channel * Conv1_input_height * Conv1_input_height * sizeof(float), cudaMemcpyHostToDevice));
-        // Conv1
-        Conv_ReLu<<<Conv1_blocksperGrid, Conv1_threadsperBlock>>>(device_input_image, device_Conv1_output_image, device_Conv1_kernel_weight, device_Conv1_kernel_bias, Conv1_input_height, Conv1_kernel_height, Conv1_output_height, Conv1_input_channel);
-        CHECK(cudaDeviceSynchronize());
-        CHECK(cudaGetLastError());
-        // MaxPool1
-        MaxPool<<<MaxPool1_blocksperGrid, MaxPool1_threadsperBlock>>>(device_Conv1_output_image, device_MaxPool1_output_image, MaxPool1_input_height, MaxPool1_output_height);
-        CHECK(cudaDeviceSynchronize());
-        CHECK(cudaGetLastError());
-        // Conv2
-        Conv_ReLu<<<Conv2_blocksperGrid, Conv2_threadsperBlock>>>(device_MaxPool1_output_image, device_Conv2_output_image, device_Conv2_kernel_weight, device_Conv2_kernel_bias, Conv2_input_height, Conv2_kernel_height, Conv2_output_height, Conv2_input_channel);
-        CHECK(cudaDeviceSynchronize());
-        CHECK(cudaGetLastError());
-        // MaxPool2
-        MaxPool<<<MaxPool2_blocksperGrid, MaxPool2_threadsperBlock>>>(device_Conv2_output_image, device_MaxPool2_output_image, MaxPool2_input_height, MaxPool2_output_height);
-        CHECK(cudaDeviceSynchronize());
-        CHECK(cudaGetLastError());
-        // Linear1
-        Linear_ReLu<<<Linear1_blocksperGrid, Linear1_threadsperBlock>>>(device_MaxPool2_output_image, device_Linear1_output_image, device_Linear1_kernel_weight, device_Linear1_kernel_bias, Linear1_input_height, true);
-        CHECK(cudaDeviceSynchronize());
-        CHECK(cudaGetLastError());
-        // Linear2
-        Linear_ReLu<<<Linear2_blocksperGrid, Linear2_threadsperBlock>>>(device_Linear1_output_image, device_Linear2_output_image, device_Linear2_kernel_weight, device_Linear2_kernel_bias, Linear2_input_height, true);
-        CHECK(cudaDeviceSynchronize());
-        CHECK(cudaGetLastError());
-        // Linear3
-        Linear_ReLu<<<Linear3_blocksperGrid, Linear3_threadsperBlock>>>(device_Linear2_output_image, device_Linear3_output_image, device_Linear3_kernel_weight, device_Linear3_kernel_bias, Linear3_input_height, false);
-        CHECK(cudaDeviceSynchronize());
-        CHECK(cudaGetLastError());
-        CHECK(cudaMemcpy(host_Linear3_output_image, device_Linear3_output_image, Linear3_output_height * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK(cudaMemcpy(device_input_image, images, batchsize * Conv1_input_height * Conv1_input_height * sizeof(float), cudaMemcpyHostToDevice));
+    // Conv1
+    Conv_ReLu<<<Conv1_blocksperGrid, Conv1_threadsperBlock>>>(device_input_image, device_Conv1_output_image, device_Conv1_kernel_weight, device_Conv1_kernel_bias, Conv1_input_height, Conv1_kernel_height, Conv1_output_height, Conv1_input_channel, Conv1_output_channel);
+    CHECK(cudaDeviceSynchronize());
+    CHECK(cudaGetLastError());
+    // MaxPool1
+    MaxPool<<<MaxPool1_blocksperGrid, MaxPool1_threadsperBlock>>>(device_Conv1_output_image, device_MaxPool1_output_image, MaxPool1_input_height, MaxPool1_output_height, MaxPool1_channel);
+    CHECK(cudaDeviceSynchronize());
+    CHECK(cudaGetLastError());
+    // Conv2
+    Conv_ReLu<<<Conv2_blocksperGrid, Conv2_threadsperBlock>>>(device_MaxPool1_output_image, device_Conv2_output_image, device_Conv2_kernel_weight, device_Conv2_kernel_bias, Conv2_input_height, Conv2_kernel_height, Conv2_output_height, Conv2_input_channel, Conv2_output_channel);
+    CHECK(cudaDeviceSynchronize());
+    CHECK(cudaGetLastError());
+    // MaxPool2
+    MaxPool<<<MaxPool2_blocksperGrid, MaxPool2_threadsperBlock>>>(device_Conv2_output_image, device_MaxPool2_output_image, MaxPool2_input_height, MaxPool2_output_height, MaxPool2_channel);
+    CHECK(cudaDeviceSynchronize());
+    CHECK(cudaGetLastError());
+    // Linear1
+    Linear_ReLu<<<Linear1_blocksperGrid, Linear1_threadsperBlock>>>(device_MaxPool2_output_image, device_Linear1_output_image, device_Linear1_kernel_weight, device_Linear1_kernel_bias, Linear1_input_height, Linear1_output_height, true);
+    CHECK(cudaDeviceSynchronize());
+    CHECK(cudaGetLastError());
+    // Linear2
+    Linear_ReLu<<<Linear2_blocksperGrid, Linear2_threadsperBlock>>>(device_Linear1_output_image, device_Linear2_output_image, device_Linear2_kernel_weight, device_Linear2_kernel_bias, Linear2_input_height, Linear2_output_height, true);
+    CHECK(cudaDeviceSynchronize());
+    CHECK(cudaGetLastError());
+    // Linear3
+    Linear_ReLu<<<Linear3_blocksperGrid, Linear3_threadsperBlock>>>(device_Linear2_output_image, device_Linear3_output_image, device_Linear3_kernel_weight, device_Linear3_kernel_bias, Linear3_input_height, Linear3_output_height, false);
+    CHECK(cudaDeviceSynchronize());
+    CHECK(cudaGetLastError());
+    CHECK(cudaMemcpy(host_Linear3_output_image, device_Linear3_output_image, batchsize * Linear3_output_height * sizeof(float), cudaMemcpyDeviceToHost));
 
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+
+    for (int t = 0; t < batchsize; t++) {
         int index = 0;
         for (int k = 0; k < 10; k++) {
-            if (host_Linear3_output_image[k] > host_Linear3_output_image[index]) {
+            if (host_Linear3_output_image[t * Linear3_output_height + k] > host_Linear3_output_image[t * Linear3_output_height + index]) {
                 index = k;
             }
         }
@@ -289,11 +293,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    cudaDeviceSynchronize();
-
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end - start;
-    std::cout << std::fixed << std::setprecision(4) << diff.count() << ":" << float(correct_nums) / float(images.size()) << std::endl;
+    std::cout << std::fixed << std::setprecision(4) << diff.count() << ":" << float(correct_nums) / float(batchsize) << std::endl;
 
     CHECK(cudaFree(device_input_image));
     // Conv1
